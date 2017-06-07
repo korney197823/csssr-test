cd7dd17053b313536bb4a2868bdc9419f911f6c2
Denis Kornienko <korney197823@gmail.com>
2017-06-07 22:55:18 +0300

    add textarea block, and made height resize function

---
+++ app/blocks/note/note.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+
+const cloneNote = () => {
+	const txt = document.querySelector('.note__area');
+	const parentElem = document.querySelector('.note');
+	const hiddenDiv = document.createElement('div');
+	let content = null;
+
+
+	txt.classList.add('noscroll');
+	hiddenDiv.classList.add('note__hidden');
+
+	parentElem.appendChild(hiddenDiv);
+
+	txt.addEventListener('keyup', () => {
+		content = txt.value;
+		content = content.replace(/\n/g, '<br>');
+
+		$(hiddenDiv).html(content);
+		$(txt).css('height', $(hiddenDiv).height());
+
+	});
+
+};
+
+export default cloneNote;
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,13 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import 'ion-rangeslider';
+import cloneNode from '../blocks/note/note';
+
 
 $(() => {
 	svg4everybody();
+	$('#example').ionRangeSlider(
+
+	);
+	cloneNode();
 });
